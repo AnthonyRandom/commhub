82dbaa639eac51bb3da3396e5a343fda9d0e4499
AnthonyRandom <anthonyrandom@proton.me>
2025-11-02 15:58:42 -0500

    test

--- packages/server/rxjs-operators-fix.js
+++
@@ -1,5 +0,0 @@
-// Compatibility module for rxjs/operators
-// This file provides backward compatibility for NestJS expecting rxjs/operators
-
-const operators = require('rxjs/dist/cjs/operators/index.js');
-module.exports = operators;

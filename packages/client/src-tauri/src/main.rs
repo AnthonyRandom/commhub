64a5ea93a760f2600dbce806cbd6137e398e2241
AnthonyRandom <anthonyrandom@proton.me>
2025-11-02 18:39:02 -0500

    fix: remove unused imports in main.rs

--- packages/client/src-tauri/src/main.rs
+++ packages/client/src-tauri/src/main.rs
@@ -1,14 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayMenu};
+use tauri::Manager;
 
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
+        .setup(|_app| {
             #[cfg(debug_assertions)]
             {
-                let window = app.get_window("main").unwrap();
+                let window = _app.get_window("main").unwrap();
                 window.open_devtools();
             }
             Ok(())
